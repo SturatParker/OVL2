44e96afc92a11de606a074af1e3a6773f5d602f8
SturatParker <seygantte@hotmail.co.uk>
2022-06-15 23:00:09 +0100

    Utils

---
+++ src/common/utils/ColourUtils.ts
@@ -0,0 +1,4 @@
+export class ColourUtils {
+  static readonly success = 1353797;
+  static readonly error = 16711680;
+}
---
+++ src/common/utils/GuildUtils.ts
@@ -0,0 +1,19 @@
+import { Guild, Message, MessageOptions, MessagePayload } from 'discord.js';
+
+export class GuildUtils {
+  static log(
+    guild: Guild,
+    options: string | MessagePayload | MessageOptions
+  ): Promise<Message> {
+    const logChannel = guild.channels.cache.find(
+      (channel) =>
+        channel.name.toLocaleLowerCase() == 'log' &&
+        channel.type == 'GUILD_TEXT'
+    );
+    if (logChannel.type != 'GUILD_TEXT')
+      return new Promise((resolve, reject) => {
+        reject(`Unable to find log channel in guild ${guild.name}`);
+      });
+    return logChannel.send(options);
+  }
+}
