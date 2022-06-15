e39fc26f0d925567be812e64d0f155e0a887dc9d
SturatParker <seygantte@hotmail.co.uk>
2022-06-15 23:01:22 +0100

    run client

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,45 @@
-import { hello, world } from 'src/subdir/hello';
+import { Client, Intents } from 'discord.js';
+import 'dotenv/config';
+import {
+  onError,
+  onGuildMemberAdd,
+  onGuildMemberRemove,
+  onReady,
+} from 'src/events';
 
-hello();
-world()
-  .then((result) => console.log(result))
-  .catch((error) => console.error(error));
+const client = new Client({
+  partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'USER'],
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.DIRECT_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+  ],
+});
+
+[onError, onGuildMemberAdd, onGuildMemberRemove, onReady].forEach((handler) => {
+  client.on(handler.event, handler.callback);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.author.bot) return;
+  if (process.env.NODE_ENV === 'production') return;
+  switch (message.content) {
+    case 'onReady':
+      onReady.callback(message.client);
+      break;
+    case 'onError':
+      onError.callback(new Error(message.content));
+      break;
+    case 'onGuildMemberAdd':
+      onGuildMemberAdd.callback(message.member);
+      break;
+    case 'onGuildMemberRemove':
+      onGuildMemberRemove.callback(message.member);
+      break;
+  }
+});
+
+client.login();
